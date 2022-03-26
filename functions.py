00815d12f1837282b393e14a1ecb95ef307f1fac
Thorkhar <100024582+Thorkhar@users.noreply.github.com>
2022-03-26 15:56:15 +0100

    added proper integrand function

--- functions.py
+++ functions.py
@@ -1,12 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-k = 10^(-23)
+k = 1.38*(10**(-23))
 T = 5700
+c = 3*(10**8)
 beta = 1/(k*T)
-h_bar = 10^(-34)
-x = np.linspace(0, 5, 100)
-y = 2/(np.exp(beta*h_bar*x)-1)
+h_bar = 10**(-34)
+x = np.linspace(0, 5, 1000)
+y = (x**2)/(np.exp(x)-1)
+
 
 plt.plot(x,y,'r')
 plt.show()
--- main.py
+++ main.py
@@ -2,14 +2,16 @@ import numpy as np
 from scipy.integrate import quad
 import matplotlib.pyplot as plt
 
-E_photon = 2.48
-k = 10^(-23)
+E_photon = 2
+k = 8.62 * 10**(-5)
 T = 5700
+c =  1 #3*(10**8)
 beta = 1/(k*T)
-h_bar = 10^(-34)
+h_bar = 1 #10**(-34)
 
 def n_integrand(x):
-    y = 2 / (np.exp(beta * h_bar * x) - 1)
+    #y = 2/(np.exp(beta*h_bar*x) - 1)
+    y = (x**2)/(np.exp(beta*x)-1)
     return y
 
 def N_integral(lower_limit, upper_limit):
@@ -17,16 +19,18 @@ def N_integral(lower_limit, upper_limit):
 
 eff_results = []
 nabs_results = []
-N_total = N_integral(0.001/h_bar, 10000000/h_bar)
+N_total = N_integral(0.001, 100000)
 print(N_total[0])
-
+print(n_integrand(0.01))
 for i in np.arange(0.001,5,0.001):
-    N_abs = N_integral(i/h_bar, 10000000/h_bar)
-    work = N_abs[0] * i
-    energy_available = N_total[0]*E_photon
-    eff = work/energy_available
+    N_abs = N_integral(i, 100000)
+    nabs = N_abs[0]/N_total[0]
+    eff = nabs * (i/E_photon)
     eff_results.append(eff)
+    nabs_results.append(nabs)
 
 plt.plot(np.arange(0.001,5,0.001), eff_results)
+plt.xlabel('Bandgap energy (eV)')
+plt.ylabel('Efficiency')
 plt.show()
 
