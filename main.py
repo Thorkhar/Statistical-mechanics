86faa7a772faca3ef8859e6a7447df8a80027599
Thorkhar <100024582+Thorkhar@users.noreply.github.com>
2022-03-27 18:07:00 +0200

    removed plot function from class

--- main.py
+++ main.py
@@ -5,41 +5,42 @@ import matplotlib.pyplot as plt
 class SolarCell:
     def __init__(self, T:int):
         self.T = T
-        self.beta = 1/(T*8.62 * 10**(-5))
-        self.E_photon = 2.701*8.62 * 10**(-5)*T
+        self.beta = 1/(T*8.62 * 10**(-5)) #set beta = 1/kT
+        self.E_photon = 2.701*8.62 * 10**(-5)*T #set the photon energy
 
-    def n_integrand(self, x):  # DoS function to integrate over
-        y = (x ** 2) / (np.exp(self.beta * x) - 1)
+    def integrand(self, x):  # DoS function to integrate over
+        y = (x ** 2) / (np.exp(self.beta * x) - 1) #can ignore constants as they drop out in the division anyway
         return y
 
-    def N_integral(self, lower_limit, upper_limit):
-        return quad(self.n_integrand, lower_limit, upper_limit)
+    def integrate(self, lower_limit, upper_limit): #integration function
+        return quad(self.integrand, lower_limit, upper_limit)
 
     def calculate_efficiency(self):
-        eff_results = []
-        N_total = self.N_integral(0.001, np.inf)
+        eff_results = [] #create empty array for the results
+        N_total = self.integrate(0.001, np.inf) #N_total integration can be kept out of the for loop to reduce computation time
 
         for i in np.arange(0.001, 5, 0.001):
-            N_abs = self.N_integral(i, np.inf)
-            nabs = N_abs[0] / N_total[0]
-            eff = nabs * (i / self.E_photon)
+            N_absorbed = self.integrate(i, np.inf)
+            n_absorbed = N_absorbed[0] / N_total[0]
+            eff = n_absorbed * (i / self.E_photon)
             eff_results.append(eff)
 
         return eff_results
 
     def calculate_max_eff(self, values_list):  # takes regular Python list as input
-        np_results_array = np.array(values_list)
+        np_results_array = np.array(values_list) #convert list to numpy array (argmax method works better than default python max() method
         max_value_index = np.argmax(np_results_array)
-        optimal_bandgap = (max_value_index + 1) * 0.001
+        optimal_bandgap = (max_value_index + 1) * 0.001 # multiply index by loop step size to get the bandgap energy
         max_value = np_results_array[max_value_index]
         return optimal_bandgap, max_value
 
-    def create_plot(self, y_values,):
-        plt.plot(np.arange(0.001, 5, 0.001), y_values, 0.001)
-        plt.xlabel('Bandgap energy (eV)')
-        plt.ylabel('Efficiency')
-        plt.show()
-
+def create_plot(y1, y2, color1, color2, name1, name2):
+    plt.plot(np.arange(0.001, 5, 0.001), y1, label=name1, color=color1)
+    plt.plot(np.arange(0.001, 5, 0.001), y2, label=name2, color=color2)
+    plt.xlabel('Bandgap energy (eV)')
+    plt.ylabel('Solar cell efficiency')
+    plt.legend(loc="upper right")
+    plt.show()
 
 sun = SolarCell(5800)
 sirius = SolarCell(9940)
@@ -47,9 +48,9 @@ sirius = SolarCell(9940)
 efficiency_sun = sun.calculate_efficiency()
 max_eff_sun, ideal_gap_sun = sun.calculate_max_eff(efficiency_sun)
 print(max_eff_sun, ideal_gap_sun)
-#sun.create_plot(efficiency_sun)
 
 efficiency_sirius = sirius.calculate_efficiency()
 max_eff_sirius, ideal_gap_sirius = sirius.calculate_max_eff(efficiency_sirius)
 print(max_eff_sirius, ideal_gap_sirius)
-sirius.create_plot(efficiency_sirius)
+
+create_plot(efficiency_sun, efficiency_sirius, 'r', 'b', 'Sun', 'Sirius A')
